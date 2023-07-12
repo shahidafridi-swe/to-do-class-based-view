0a453fb23e666b0d26146f44e32db8e6b76c4cc6
shahidafridi-swe <shahidafridi.cse@gmail.com>
2023-07-12 22:01:17 +0600

    ready

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s96p#!8c)-42bys$ku-l)9(l^3s^&@$=b*vjnhtt6=zer@1nq#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
